c759e95178fff089be6e893f1511a9fb3ef4b5f1
wielkieG <pawelgrobelak@interia.pl>
2019-10-29 13:00:57 +0100

    ktory to wiek

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# program pokazuje ktory to wiek
+
+def century(year):
+    if year<101:
+        return 1
+    wiek = year//100
+    if year%100 != 0:
+        wiek += 1
+    return wiek
+
+
+if __name__ == '__main__':
+    print(century(211))
